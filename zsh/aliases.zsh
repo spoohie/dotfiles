2a19f8c423fcad44b93ee32fab6d0791f0428c8f
spoohie <kplatkowski@outlook.com>
2023-01-26 16:49:47 +0100

    Add git add -u alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -31,7 +31,8 @@ alias ctar="tar -cvzf"
 
 # git
 alias gs="git status"
-alias ga="git add -u"
+alias ga="git add"
+alias gu="git add -u"
 alias gcm="git commit -m"
 alias gcp="git cherry-pick"
 alias gitl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
